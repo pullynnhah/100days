217c971883a58cd3000fc50e647e0bbcc959e625
Paula Rodrigues <pullynnhah@icloud.com>
2021-06-08 15:09:04 -0300

    add a left paddle

--- days021-030/day022/project/pong_game.py
+++ days021-030/day022/project/pong_game.py
@@ -10,10 +10,14 @@ screen.title("🕹 Pong ️🕹️")
 screen.tracer(0)
 
 right_paddle = Paddle((350, 0))
+left_paddle = Paddle((-350, 0))
 
 screen.listen()
 screen.onkey(right_paddle.up, 'Up')
 screen.onkey(right_paddle.down, 'Down')
+screen.onkey(left_paddle.up, 'w')
+screen.onkey(left_paddle.down, 's')
+
 
 game_over = False
 
