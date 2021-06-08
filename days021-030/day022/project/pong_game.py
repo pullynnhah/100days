d72c2e1433d414d33ce2fbb0412def3192fec493
Paula Rodrigues <pullynnhah@icloud.com>
2021-06-08 14:00:53 -0300

    create a paddle class

--- days021-030/day022/project/pong_game.py
+++ days021-030/day022/project/pong_game.py
@@ -1,10 +1,23 @@
 from turtle import Screen
 
+from paddle import Paddle
+
 screen = Screen()
 
 screen.setup(width=800, height=600)
 screen.bgcolor('black')
 screen.title("🕹 Pong ️🕹️")
 screen.tracer(0)
+
+right_paddle = Paddle((350, 0))
+
+screen.listen()
+screen.onkey(right_paddle.up, 'Up')
+screen.onkey(right_paddle.down, 'Down')
+
+game_over = False
+
+while not game_over:
+    screen.update()
 
 screen.exitonclick()
