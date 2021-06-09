c75782d53581618e4fa3adfba1f8179d38e2aeb5
Paula Rodrigues <pullynnhah@icloud.com>
2021-06-09 14:25:14 -0300

    intro to Tkinter

---
+++ days021-030/day027/lectures/lec01.py
@@ -0,0 +1,11 @@
+import tkinter
+
+window = tkinter.Tk()
+
+window.title("🖱️ My first GUI program 🖱️")
+window.minsize(width=500, height=300)
+
+label = tkinter.Label(text='I am a Label', font=("Atari Classic Extrasmooth", 12))
+label.pack()
+
+window.mainloop()
