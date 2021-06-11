98347577a2884945b3a5e5f6b40b765edf6f227e
Paula Rodrigues <pullynnhah@icloud.com>
2021-06-10 21:35:08 -0300

    examples of errors

---
+++ days021-030/day030/lectures/lec01/main.py
@@ -0,0 +1,18 @@
+# FileNotFoundError
+# with open('file.txt') as file:
+#     file.read()
+
+dictionary = {"key": "value"}
+
+# KeyError
+# value = dictionary['non_existent_key']
+
+fruits = ["Apple", "Banana", "Pear"]
+
+# IndexError
+# fruit = fruits[3]
+
+text = "abc"
+
+# TypeError
+# print(text + 5)
