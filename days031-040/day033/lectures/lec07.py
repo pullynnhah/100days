3fca1b3504980206a3d7ffabac6afbe04f52ea4c
Paula Rodrigues <pullynnhah@icloud.com>
2021-06-12 13:09:17 -0300

    api with parameters

---
+++ days031-040/day033/lectures/lec07.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+import requests
+
+
+def hour_time(time):
+    return int(time.split("T")[1].split(":")[0])
+
+
+LATITUDE = 51.507351
+LONGITUDE = -0.127758
+URL = "https://api.sunrise-sunset.org/json"
+
+params = {
+    "lat": LATITUDE,
+    "lng": LONGITUDE,
+    "formatted": 0
+}
+response = requests.get(URL, params=params)
+response.raise_for_status()
+data = response.json()
+print(data)
+
+sunrise = hour_time(data['results']['sunrise'])
+sunset = hour_time(data['results']['sunset'])
+
+
+now = datetime.now()
+
+print(sunrise, sunset)
