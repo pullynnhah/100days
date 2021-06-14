e27a12c4217e1f1332b7cdbc71f0873170ec5291
Paula Rodrigues <pullynnhah@icloud.com>
2021-06-13 21:12:20 -0300

    refactor whole code based on video

--- days031-040/day036/steps/ste01.py
+++ days031-040/day036/steps/ste01.py
@@ -1,30 +1,9 @@
 import os
-
-from datetime import datetime, timedelta
-
 import requests
-
-
-def get_yesterday(date):
-    weekday = date.weekday()
-    if weekday == 0:
-        return date - timedelta(days=3)
-    if weekday == 6:
-        return date - timedelta(days=2)
-    return date - timedelta(days=1)
 
 
 def format_date(date):
     return date.strftime('%Y-%m-%d')
-
-
-def get_value(data, date):
-    return float(data["Time Series (Daily)"][date]["4. close"])
-
-
-def get_percentage(value, previous_value):
-    return (value - previous_value) * 100 / value
-
 
 STOCK = "TSLA"
 
@@ -34,18 +13,13 @@ params = {
     'apikey': os.getenv('ALPHA_KEY')
 }
 
-today = datetime.now()
-yesterday_raw = get_yesterday(today)
-before_yesterday_raw = get_yesterday(yesterday_raw)
+response = requests.get('https://www.alphavantage.co/query', params=params).json()["Time Series (Daily)"]
+data = [value for value in response.values()]
 
-yesterday = format_date(yesterday_raw)
-before_yesterday = format_date(before_yesterday_raw)
+yesterday_value = float(data[0]['4. close'])
+before_yesterday_value = float(data[1]['4. close'])
 
-response = requests.get('https://www.alphavantage.co/query', params=params).json()
-
-yesterday_value = get_value(response, yesterday)
-before_yesterday_value = get_value(response, before_yesterday)
-percentage = get_percentage(yesterday_value, before_yesterday_value)
+percentage = (yesterday_value - before_yesterday_value) * 100 / yesterday_value
 
 if not (-5 <= percentage <= 5):
     print("Get News")
