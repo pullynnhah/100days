68100eff8b848b595faa4d660ed8ce514375b42f
Paula Rodrigues <pullynnhah@icloud.com>
2021-06-14 07:31:53 -0300

    adding pixel using datetime strftime

---
+++ days031-040/day037/lectures/lec04.py
@@ -0,0 +1,21 @@
+import os
+
+from datetime import datetime
+
+import requests
+
+USERNAME = 'paulaabrodrigues'
+TOKEN = os.getenv('PIXELA_TOKEN')
+GRAPH_ID = 'graph1'
+
+pixela_endpoint = 'https://pixe.la/v1/users'
+pixel_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}'
+
+day = datetime(year=2021, day=12, month=6)
+pixel_config = {
+    'date': day.strftime("%Y%m%d"),
+    'quantity': '0'
+}
+
+response = requests.post(url=pixel_endpoint, json=pixel_config, headers={"X-USER-TOKEN": TOKEN})
+print(response.text)
