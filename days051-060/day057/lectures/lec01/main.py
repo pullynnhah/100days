d7a91fb018a3857665e6a3db4d230dbf974769a5
pullynnhah <pullynnhah@icloud.com>
2021-06-21 04:33:55 -0300

    lectures on flask using jinja

--- days051-060/day057/lectures/lec01/main.py
+++ days051-060/day057/lectures/lec01/main.py
@@ -1,6 +1,9 @@
 from random import randint
 from datetime import datetime
+
+import requests
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
@@ -10,6 +13,12 @@ def home():
     curr_year = datetime.now().year
     return render_template('index.html', num=random_number, year=curr_year)
 
+
+@app.route('/blog/<num>')
+def get_blog(num):
+    print(num)
+    data = requests.get('https://api.npoint.io/780aa5562e9f8266cc96').json()
+    return render_template('blog.html', posts=data)
 
 
 if __name__ == '__main__':
