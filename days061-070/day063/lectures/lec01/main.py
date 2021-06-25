6617968c3a779f898499246f7b4fa18b6b02a04e
pullynnhah <pullynnhah@icloud.com>
2021-06-25 02:56:44 -0300

    use sqlite3

---
+++ days061-070/day063/lectures/lec01/main.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+db = sqlite3.connect('books-collection.db')
+cursor = db.cursor()
+
+cursor.execute("""
+CREATE TABLE books (
+    id INTEGER PRIMARY KEY,
+    title varchar(250) NOT NULL UNIQUE,
+    author varchar(250) NOT NULL,
+    rating FLOAT NOT NULL
+)""")
+
+cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+db.commit()
